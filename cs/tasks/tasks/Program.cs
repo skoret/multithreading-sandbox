93a87687a9c1043803b863c48f0dfa224e991752
Vadim Volodin <vad.e.volodin@gmail.com>
2021-09-16 05:57:53 +0300

    C# Tasks

---
+++ cs/tasks/tasks/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = Task<int>.Factory.StartNew( () => {
+                Thread.Sleep(1000);
+                return 5;
+            } );
+            Console.WriteLine("Just after starting task");
+            Console.WriteLine("Result is: {0}", t.Result);
+        }
+    }
+}
