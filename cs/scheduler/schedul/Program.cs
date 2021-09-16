629150e199b2c5405d3a8edc80166df29a5643b4
Vadim Volodin <vad.e.volodin@gmail.com>
2021-09-16 06:06:32 +0300

    C# Scheduler

---
+++ cs/scheduler/schedul/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace schedul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0; 
+            
+            TimerCallback tm = new TimerCallback((object Object) =>
+            {
+                Console.WriteLine("Hello timer!!!");
+            });
+
+            Timer timer = new Timer(tm, num, 0, 1000);
+ 
+            Thread.Sleep(5000);        
+        }
+    }
+}
